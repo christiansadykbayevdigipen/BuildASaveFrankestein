2451b5713b94a308b27fb3a675987bc5e7bdde92
zaza961 <155696145+zaza961@users.noreply.github.com>
2025-03-14 14:51:30 -0700

    Finished basic voltage system

--- Phrankenstein_s Phrankeria/Assets/Scripts/Zaden's scripts/Voltage MIni Game.cs
+++ Phrankenstein_s Phrankeria/Assets/Scripts/Zaden's scripts/Voltage MIni Game.cs
@@ -14,18 +14,26 @@ public class VoltageMIniGame : MonoBehaviour
     //Public bools
     public Transform pivotPoint;
     public Transform linePart;
-    public float rotationSpeed = -50f;
+    public float rotationSpeed = 50f;
     public float radius = 2f;
     public float maxAngle = 90f;
     public float minAngle = -90f;
     public float currentAngle = 0;
+    public float startingPoint = 45f;
+    public Quaternion startRotation = Quaternion.identity;
     //public Transform Win;
 
+
     //Privet bools
-    
+    private Vector3 startPoint;
     
     //Edit points
 
+    private void Start()
+    {
+        startPoint = transform.position;
+        Debug.Log("Start Position: " + startPoint);
+    }
 
     private void Update()
     {
@@ -38,11 +46,13 @@ public class VoltageMIniGame : MonoBehaviour
 
         float raidianAngel = currentAngle * Mathf.Rad2Deg;
 
+        float zRotation = transform.eulerAngles.z;
+
         //Vector2 newPosition = new Vector2(pivotPoint.position.x + Mathf.Cos(raidianAngel) * pivotPoint.y + Mathf.Sin(raidianAngel) * radius);
 
         //transform.position = newPosition; 
 
-        if(pivotPoint != null)
+        if (pivotPoint != null)
         {
             transform.RotateAround(pivotPoint.position, Vector3.forward, rotationSpeed * Time.deltaTime);
         }
@@ -54,12 +64,53 @@ public class VoltageMIniGame : MonoBehaviour
             rotationSpeed = -rotationSpeed;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             linePart.gameObject.CompareTag("Win thing");
             Debug.Log("woring for now");
         }
+
+        if (rotationSpeed != 50f && zRotation > 179 && zRotation < 181)
+        {
+            Debug.Log($"rotationSpeed: {rotationSpeed}, zRotation: {zRotation}");
+            Debug.Log("180's working");
+            rotationSpeed = -rotationSpeed;
+        }
+
+        if (rotationSpeed != -50 && zRotation > 0.1 && zRotation < 1)
+        {
+            Debug.Log("0's working");
+            rotationSpeed = -rotationSpeed;
+        }
+
+        /*if (rotationSpeed != -50 && zRotation > 1 && zRotation < -1)
+        {
+            Debug.Log($"rotationSpeed: {rotationSpeed}, zRotation: {zRotation}");
+            rotationSpeed = -rotationSpeed; 
+        }*/
+
+        /*if (rotationSpeed != -50 && zRotation > 1 && zRotation <  -1)
+        {
+            Debug.Log("0's working");
+            rotationSpeed = -rotationSpeed;
+        }*/
+
+        /* if (Mathf.Approximately(zRotation,0))
+         {
+             Debug.Log("0's working");
+             rotationSpeed = -rotationSpeed;
+         }*/
     }
+
+   /* private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (rotationSpeed != -50)
+        {
+            Debug.Log("0's working");
+            rotationSpeed = -rotationSpeed;
+        }
+    }*/
+
 
     /*private void OnTriggerEnter2D(Collider2D other)
     {
